3480f7fb54aa8aff585b89ffbd4ee0529ab80a29
Vladimir <asetr95@live.com>
2023-09-25 17:52:12 +0300

    Add FriendRepository interface

--- SocialNetwork/DAL/Repositories/FriendRepository.cs
+++ SocialNetwork/DAL/Repositories/FriendRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SocialNetwork.DAL.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,31 @@ using System.Threading.Tasks;
 
 namespace SocialNetwork.DAL.Repositories
 {
-    internal class FriendRepository
+    public class FriendRepository : BaseRepository, IFriendRepository
     {
+        public int Create(FriendEntity friendEntity)
+        {
+            return Execute(@"insert into friends 
+                            (userId, friendId) 
+                            values (:user_id, :friend_id)", friendEntity);
+        }
+
+        public int Delete(int id)
+        {
+            return Execute(@"delete from friends 
+                            where friendId = :id_p", new {id_p = id});
+        }
+
+        public IEnumerable<FriendEntity> FindAllbyUserId(int userId)
+        {
+            return Query<FriendEntity>(@"select * from friends where userId = :user_id", new { user_id = userId });
+        }
+    }
+
+    public interface IFriendRepository
+    {
+        int Create(FriendEntity friendEntity);
+        IEnumerable<FriendEntity> FindAllbyUserId(int userId);
+        int Delete(int id);
     }
 }
